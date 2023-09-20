cae0a6ab6b896d6ed4e09a527ea7133762e1d97f
Quinn-Kiwalabye <kiwalabyequinn00@gmail.com>
2023-09-20 02:08:53 +0200

    color change

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   development: {
-    backendUrl: "http://shirt-maker-k3vb.onrender.com/api/v1/dalle",
+    backendUrl: "http://shirt-maker-k3vb.onrender.com/api/v1/dalle", //added /api/v1/dalle
   },
   production: {
     backendUrl: "https://shirt-maker-k3vb.onrender.com/api/v1/dalle",
